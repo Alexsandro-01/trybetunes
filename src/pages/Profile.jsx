6fa86ff2c83731f0c75b7d3e0cdb84218362894a
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-03-08 17:44:18 -0300

    requisito 13 concluído

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      profile: {},
+    };
+  }
+
+  componentDidMount() {
+    this.findUser();
+  }
+
+  findUser = () => {
+    this.setState({
+      loading: true,
+    }, async () => {
+      this.setState({
+        loading: false,
+        profile: await getUser(),
+      });
+    });
+  }
+
   render() {
+    const { profile, loading } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {
+          loading ? <Loading /> : (
+            <section>
+              <div className="img-user">
+                <img
+                  data-testid="profile-image"
+                  src={ profile.image }
+                  alt={ profile.name }
+                />
+                <button type="button">
+                  <Link to="/profile/edit">
+                    Editar perfil
+                  </Link>
+                </button>
+              </div>
+              <div className="name-user">
+                <p><strong>Nome:</strong></p>
+                <p>{profile.name}</p>
+              </div>
+              <div className="email-user">
+                <p><strong>Email:</strong></p>
+                <p>{profile.email}</p>
+              </div>
+              <div className="description-user">
+                <p><strong>Descrição:</strong></p>
+                <p>{profile.description}</p>
+              </div>
+            </section>
+          )
+        }
       </div>
     );
   }
